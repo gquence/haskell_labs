2665929cddff24d83299dbea1c387d6dcf728a15
gquence <ma.anshukov@gmail.com>
2018-12-11 11:34:04 +0300

    rm some files

--- EXAM/ft.h
+++
@@ -1,6 +0,0 @@
-ft_translate :: Int -> [Char]
-ft_translate _ = "that's all, folks"
-ft_translate n
-    | n mod 10 == 0 = "zero " ft_translate (n div 10) 
-    | n mod 10 == 1 = "one " ft_translate (n div 10)
-    | otherwise = "other"
