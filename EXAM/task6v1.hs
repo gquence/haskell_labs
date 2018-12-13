9dcb61aa927b653c7c61de5edcc2a0c3245b0002
Mihail <gquence@d4a.localdomain>
2018-12-13 17:06:35 +0300

    only + and -

---
+++ EXAM/task6v1.hs
@@ -0,0 +1,26 @@
+import Data.List
+--  только при условии, что "=" в конце
+ft_eq :: [Int] -> [Char]
+ft_eq [] = "wrong values"
+ft_eq [x,y]
+    | x == y    = (show x) ++ (" = ") ++ (show y)
+    | otherwise = "!"
+ft_eq (x : xs)
+    | last (ft_equation (x : xs)) == '!' = "wrong values"
+    | otherwise = (show x) ++ (ft_equation (x : xs))
+
+ft_equation :: [Int] -> [Char]
+ft_equation [x, y, z]
+    | x == y && y == z = (" = ") ++ (show y) ++ (" = ") ++ (show z)
+    | ((+) x y) == z =  (" + ") ++ (show y) ++ (" = ") ++ (show z)
+    | ((-) x y) == z =  (" - ") ++ (show y) ++ (" = ") ++ (show z)
+--    | ((*) x y) == z =  (" * ") ++ (show y) ++ (" = ") ++ (show z)
+--    | ((/) x y) == z =  (" / ") ++ (show y) ++ (" = ") ++ (show z)
+    | otherwise = "!"
+ft_equation (x : y : xs)
+    | last (ft_equation ((+) x y : xs)) /= '!' =  (" + ") ++ (show y) ++ (ft_equation (((+) x y) : xs))
+    | last (ft_equation ((-) x y : xs)) /= '!' = (" - ") ++ (show y) ++ (ft_equation (((-) x y) : xs))
+--    | last (ft_equation ((*) x y : xs)) /= '!' = (" * ") ++ (show y) ++ (ft_equation (((*) x y) : xs))
+--    | last (ft_equation ((/) x y : xs)) /= '!' = (" / ") ++ (show y) ++ (ft_equation (((/) x y) : xs))
+    | (x == y) && last (ft_equation (y : xs)) /= '!' = (" = ") ++ (show y) ++ (ft_equation (y : xs))
+    | otherwise = "!"
