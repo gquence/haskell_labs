5c79c2ef7b5e5706f3c4e7dc11da8593bda22e5a
gquence <ma.anshukov@gmail.com>
2021-02-11 18:09:41 +0300

    fibonachi tasks almost done; myDiv now can work with negative arguments

---
+++ log_prog/lab3/fib.hs
@@ -0,0 +1,16 @@
+fibN_naive :: Integer -> Integer
+fibN_naive x
+    | (x == 1) || (x == 2) = 1
+    | x > 2 = fibN_naive (x - 1) + fibN_naive (x - 2)
+    | otherwise = error "fibN_naive: Input number must be lower than 0"
+
+fibN :: Integer -> Integer
+fibN x | x < 0 = error "fibN: Input number must be lower than 0"
+fibN x = fibn x
+  where
+    fibn 1 = 1
+    fibn 2 = 1
+    fibn n  = (fibn (n-1)) + (fibn (n-2))
+
+fib_map :: [Int]
+fib_map = map fibN [1,2 ..]
--- log_prog/lab3/myDiv.hs
+++ log_prog/lab3/myDiv.hs
@@ -1,11 +1,13 @@
 
 myDiv :: Int -> Int -> (Int, Int)
 myDiv x y
+    | (x < 0) && (y < 0) = myDiv (-x) (-y)
+    | (x < 0) || (y < 0) = ((-fst_res), sec_res)
     | x >= y = (fst_res, sec_res)
     | otherwise = (0, x)
     where
-        res_tuple = myDiv (x - y) y
+        res_tuple = myDiv ((abs x) - y) y
         fst_res = fst(res_tuple) + 1
         sec_res = snd(res_tuple)
-
+    
         
