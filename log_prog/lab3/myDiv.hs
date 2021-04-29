73391a18840d68e0c3fe7036d155d76a9b23a8cb
gquence <ma.anshukov@gmail.com>
2021-04-29 11:54:16 +0300

    corrected myDiv.hs in therd lab

--- log_prog/lab3/myDiv.hs
+++ log_prog/lab3/myDiv.hs
@@ -2,10 +2,10 @@
 myDiv :: Integer -> Integer -> (Integer, Integer)
 myDiv x y
     | y == 0 = error "divide by zero"
-    | (x < 0) && (y > 0) && ((abs x) > (abs y)) = ((-fst_res), (-sec_res))
-    | (x < 0) && (y < 0) && ((abs x) > (abs y)) = (fst_res, (-sec_res))
-    | (x > 0) && (y < 0) && ((abs x) > (abs y)) = ((-fst_res), sec_res) 
-    | (x >= y) && ((abs x) > (abs y)) = (fst_res, sec_res)
+    | (x < 0) && (y > 0) && ((abs x) >= (abs y)) = ((-fst_res), (-sec_res))
+    | (x < 0) && (y < 0) && ((abs x) >= (abs y)) = (fst_res, (-sec_res))
+    | (x > 0) && (y < 0) && ((abs x) >= (abs y)) = ((-fst_res), sec_res) 
+    | (x >= y) && ((abs x) >= (abs y)) = (fst_res, sec_res)
     | otherwise = (0, x)
     where
         res_tuple = myDiv ((abs x) - (abs y)) (abs y)
